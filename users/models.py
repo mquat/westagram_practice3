40c69077186960672d1e55cc98d12d9384cf8af5
mquat <meeryuyu@gmail.com>
2022-03-27 21:58:04 +0900

    Add: users 앱 추가 및 모델링 작성 완료

---
+++ users/models.py
@@ -0,0 +1,13 @@
+from django.db import models 
+from django.views import View
+
+class User(models.Model):
+    user         = models.CharField(max_length=100)
+    email        = models.CharField(max_length=100, unique=True)
+    password     = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'users'
